273c90c69cf238d41a8f6c6f8f214f1b7fa30abb
Rahul182438 <88880542+Rahul182438@users.noreply.github.com>
2021-10-18 10:50:11 +0530

    Add files via upload

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,22 +1,41 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MobileVerification from '../screens/MobileOtp/mobileVerification';
 import Home from '../screens/MobileOtp/Categories';
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { View, Text, Image } from 'react-native'
+import { Icon } from 'react-native-elements';
 
-
+import TopTabNavigation from '../components/customNavigation/topNavigator/topTabNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 
 const LoginStack=()=>{
 
     const Stack = createNativeStackNavigator();
-
+    
+ 
     return(
+        <NavigationContainer>
         <Stack.Navigator>
             
-            <Stack.Screen name="Login"  component={MobileVerification} />
-            <Stack.Screen name="Categories"  component={Home} />
-        </Stack.Navigator>
+            <Stack.Screen name="Login"  options={{headerShown:false}}   component={MobileVerification} />
+            <Stack.Screen name="Categories"  options={{headerShown:false}}  component={TopTabNavigation} 
+                options={{
+                    title:'Fruits & Vegetables',
+                    headerRight: ()=>{ 
+                            return(
+                                <View>
+                                      <Image resizeMode='contain' source={require('../assets/images/Vector.png')} />
+                                    {/* <Icon size={21} name="star" type="material"/> */}
+                                </View>
+                            )
+                        }
+                    }} />
+        </Stack.Navigator>   
+        </NavigationContainer>             
+            
     )
+
 }
 
 export default LoginStack
