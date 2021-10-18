9b13eaface064f057ab51a12746f6b047b26402b
Rahul182438 <88880542+Rahul182438@users.noreply.github.com>
2021-10-18 10:47:47 +0530

    Add files via upload

--- src/assets/themes/colors.js
+++ src/assets/themes/colors.js
@@ -1,9 +1,26 @@
-export default {
+// export default {
+//     primary: '#4361ee',
+//     secondary: '#3f37c9',
+//     accent: '#4895ef',
+//     danger: '#f72585',
+//     success: '#4cc9f0',
+//     grey: '#adb5bd',
+//     white: '#ffffff',
+// }
+
+const COLORS = {
+    white: '#fff',
+    dark: '#000',
+    red: '#F52A2A',
+    light: '#F1F1F1',
+    green: '#00B761',
     primary: '#4361ee',
     secondary: '#3f37c9',
     accent: '#4895ef',
     danger: '#f72585',
     success: '#4cc9f0',
     grey: '#adb5bd',
-    white: '#ffffff',
-}
+    white: '#ffffff'
+  };
+  
+  export default COLORS;
